a47bce57c43f7d8ae72e5f21170d1d9f9fe29c61
jeuub <jeuub.bodur@gmail.com>
2021-05-23 17:09:31 +0300

    error handlers

---
+++ server/handlers/index.js
@@ -0,0 +1,11 @@
+module.exports.notFound = (req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404; 
+  next(err);
+};
+
+module.exports.errors = (err, req, res, next) => {
+  res.status(err.status | 500).json({
+    err: err.message || 'something went wrong'
+  });
+};
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const handle = require('./handlers');
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => res.json({ hello: 'world' }));
+
+app.use(handle.notFound);
+
+app.use(handle.errors);
+
+app.listen(port, console.log(`Server started on port: ${port}`));
